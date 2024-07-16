f988afa817d45052e59a9ad9eb1ec5832b47dc96
Ivan Hlieb <2020ivanko14@gmail.com>
2024-07-16 15:30:06 +0200

    deleted and finish upd

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,12 @@
 function formatMessage(message, maxLength) {
-    const messageLength = message.length;
-    if (maxLength >= messageLength) {
+    const length = message.length;
+    if (length <= maxLength) {
         return message;
     } else {
         return message.slice(0, maxLength) + '...';
     }
 }
+
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
